59a3e02cd84bd2dc93ef854795db655e0b18306b
Miqueias <miqueiazmendesarrais@gmail.com>
2024-02-25 14:14:48 -0300

    subindo meu projeto

---
+++ script.js
@@ -0,0 +1,24 @@
+function playprato(){
+    let audio=document.getElementById("soundprato")
+    audio.play()
+}
+function playbumbo(){
+    let audio=document.getElementById("soundbumbo")
+    audio.play()
+}
+function playcaixa(){
+    let audio=document.getElementById("soundcaixa")
+    audio.play()
+}
+function playtomum(){
+    let audio=document.getElementById("soundtomum")
+    audio.play()
+}
+function playtomdois(){
+    let audio=document.getElementById("soundtomdois")
+    audio.play()
+}
+function playsurdo(){
+    let audio=document.getElementById("soundsurdo")
+    audio.play()
+}
